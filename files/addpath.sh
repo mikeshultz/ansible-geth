981fb87220f064183ee0368311f63c010ab9507e
Mike Shultz <shultzm@gmail.com>
2017-07-24 13:21:03 -0600

    addpath.sh now modifies .bash_profile

--- files/addpath.sh
+++ files/addpath.sh
@@ -3,4 +3,5 @@
 # Add a PATH if it isn't already there
 if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
     PATH="${PATH:+"$PATH:"}$1"
+    echo "export PATH=${PATH:+"$PATH:"}$1" >> ~/.bash_profile
 fi
